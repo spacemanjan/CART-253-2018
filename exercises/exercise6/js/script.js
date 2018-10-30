d1266ddefc78cd1d73e9a8927792d3df6f58706c
Yann-Maurice McNiven <Yanmaumcniven@gmail.com>
2018-10-30 16:20:22 -0400

    Ex6. FIXED CODE
    Update was missing ()

--- exercises/exercise6/js/script.js
+++ exercises/exercise6/js/script.js
@@ -42,8 +42,8 @@ function draw() {
 
   leftPaddle.handleInput();
   rightPaddle.handleInput();
-
-  ball.update;
+//========FIXED===================///
+  ball.update();
   leftPaddle.update();
   rightPaddle.update();
 
