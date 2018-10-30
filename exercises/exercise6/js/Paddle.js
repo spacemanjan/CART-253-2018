854647fd833de9d6e09bc965515d26124fbc7d65
Yann-Maurice McNiven <Yanmaumcniven@gmail.com>
2018-10-30 16:00:55 -0400

    Ex6. Mispelt display and extra open bracket
    In Paddle.prototype.display, display was spelt wrong, and function had an extra open bracket.

--- exercises/exercise6/js/Paddle.js
+++ exercises/exercise6/js/Paddle.js
@@ -44,6 +44,7 @@ Paddle.prototype.update = function() {
 // display()
 //
 // Draw the paddle as a rectangle on the screen
-Paddle.prototype.disploy = function()) {
-  rectangle(this.x,this.y,this.w,this.h);
+//========FIXED===================///
+Paddle.prototype.display = function() {
+  rectangle(this.x, this.y, this.w, this.h);
 }
