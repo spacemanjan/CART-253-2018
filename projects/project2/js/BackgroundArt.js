b3a67bad5818dd3d70ad6bc855ef62a6d6998c72
Yann-Maurice McNiven <Yanmaumcniven@gmail.com>
2018-11-05 19:26:33 -0500

    P2. Added Background Visuals
    This took some fucking time, originally I wanted to make the background in script using the while loop but I couldn't figure out how to have the stars display without reshuffling everything (i.e. static). Settled on making an array with 7 background images stored in it, and a random selector that picks from the list at the start of every game. Reshuffles after a restart.

---
+++ projects/project2/js/BackgroundArt.js
@@ -0,0 +1,33 @@
+// BackgroundArt
+//
+// Class which picks from an array which starry background will be used.
+// Impoves the visuals of the game.
+// declare our 2 variables.
+var starryIndex;
+var starrySky;
+
+// BackgroundArt constructor
+//
+// sets the properties for our background.
+function BackgroundArt(x,y,w,h) {
+this.x = x;
+this.y = y;
+this.w= w;
+this.h = h;
+}
+
+//starBackg.selection();
+//
+//picks at random from the array stars declared in the script.js
+//sets the starrySky variable as the randomly chosen background.
+BackgroundArt.prototype.selection = function(){
+  starryIndex = floor(random(0,stars.length));
+  starrySky = stars[starryIndex];
+}
+
+// BackgroundArt.display()
+//
+// Displays starry space background.
+BackgroundArt.prototype.display = function(){
+image(starrySky, this.x, this.y, this.w, this.h);
+}
--- projects/project2/js/script.js
+++ projects/project2/js/script.js
@@ -12,6 +12,8 @@
 // Written with JavaScript OOP.
 
 // Variable to contain the objects representing our ball and paddles
+var stars = [];
+var starBackg;
 var leftPaddle;
 var leftShot;
 var leftScore;
@@ -21,36 +23,50 @@ var rightScore;
 var aliens;
 var ball;
 var badBall = [];
+
+//preload()
+function preload(){
+  stars = [loadImage("assets/images/stars1.png"),loadImage("assets/images/stars2.png"),
+  loadImage("assets/images/stars3.png"),loadImage("assets/images/stars4.png"),
+  loadImage("assets/images/stars5.png"),loadImage("assets/images/stars6.png"),
+  loadImage("assets/images/stars7.png")];
+}
+
 // setup()
 //
 // Creates the ball and paddles
 function setup() {
   createCanvas(640, 480);
+  // Create background stars
+  //BackgroundArt (x, y, size, numStar)
+  starBackg = new BackgroundArt(0, 0, 640, 480);
   // Create a ball
   ball = new Ball(width / 2, height / 2, 5, 5, 10, 5);
   // Create the left paddle with W and S as controls
   // Keycodes 83 and 87 are W and S respectively
   leftPaddle = new Paddle(0, height / 2, 10, 60, 10, 83, 87, 0);
   // Create the left Paddle shot with the SPACE key as a trigger
-  // Shooter(x, y, vx, w, h, speed, shootKey, ammo)
+  // Shooter (x, y, vx, w, h, speed, shootKey, ammo)
   leftShot = new Shooter(leftPaddle.x, leftPaddle.y, 10, 30, 10, 10, 32, leftPaddle.score, false);
   // Create score board for the left Paddle
-  //(x, y, size, score, spacing)
+  //ScoreManager (x, y, size, score, spacing)
   leftScore = new ScoreManager(30, 25, 15, 20);
   // Create the right paddle with UP and DOWN as controls
   rightPaddle = new Paddle(width - 10, height / 2, 10, 60, 10, DOWN_ARROW, UP_ARROW, 0);
   // Create the right Paddle shot with the SHIFT key as a trigger
-  // Shooter(x, y, xv, w, h, speed, shootKey, ammo)
+  // Shooter (x, y, xv, w, h, speed, shootKey, ammo)
   rightShot = new Shooter(rightPaddle.x-10, rightPaddle.y, 10, 30, 10, -10, 16, rightPaddle.score, false);
   // Create score board for the right Paddle
-  //(x, y, size, score, spacing)
+  // ScoreManager (x, y, size, score, spacing)
   rightScore = new ScoreManager(width-45, 25, 15, -20);
   // Create aliens with autonomous controls
-  //Aliens(x, y, vx, vy, size, speed, capture, score)
+  //Aliens (x, y, vx, vy, size, speed, capture, score)
   aliens = new Aliens(250, 10, 1, -1, 50, 0.010, false, false, 0);
   // initiates the title feature
-  //  UP_ARROW to start DOWN_ARROW to restart after game over.
+  // UP_ARROW to start DOWN_ARROW to restart after game over.
   title = new Title(true, false);
+  //
+  //
 }
 
 // draw()
@@ -73,15 +89,14 @@ function draw() {
     }
   } else {
     // if it's not a game over and title.start is true display start screen.
-    // else time to play.
+    // BackgroundArt selects a new random star background, else time to play.
     if (title.start === true) {
       background(100);
       title.display();
+      starBackg.selection();
     } else {
-
-
       //=========GAME START======================//
-      background(0);
+      starBackg.display();
       //=========LEFT PADDLE=====================//
       leftPaddle.handleInput();
       leftShot.handleInput(leftPaddle);
